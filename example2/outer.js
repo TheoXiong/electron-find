0b70f0ce5d08a1c548142fb5944d6393fd032894
zhi.xiong <zhi.xiong@xtalpi.com>
2019-06-04 11:32:46 +0800

    update example2

--- example2/outer.js
+++ example2/outer.js
@@ -1,31 +1,24 @@
-const { remote, ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron')
 const { FindInPage } = require('../src/index.js')
 
 let findInPage = null
 const webview1 = document.querySelector('#webview1')
 webview1.addEventListener('dom-ready', () => {
   findInPage = new FindInPage(webview1.getWebContents())
-  ipcRenderer.on('on-find', (e, args) => {
-    findInPage.openFindWindow()
-  })
 })
 webview1.addEventListener('close', () => {
-  console.log('webview1 close', )
   if (findInPage) {
     findInPage.destroy()
     findInPage = null
   }
 })
 webview1.addEventListener('destroyed', () => {
-  console.log('webview1 destroyed', )
   if (findInPage) {
     findInPage.destroy()
     findInPage = null
   }
 })
-
 webview1.addEventListener('crashed', () => {
-  console.log('webview1 crashed', )
   if (findInPage) {
     findInPage.destroy()
     findInPage = null
